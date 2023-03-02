8ad83cafa84e9973f2889f9a7db97158a372552d
PaperCut <ronny.zhang@amperetime.com>
2023-03-02 10:50:07 +0800

    feat: Init

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    title: " Ronny Zhang",
+    description: "分享 Ronny Zhang 的相关博客。",
+    author: "Ronny Zhang",
+    avatar:
+        "https://p3-passport.byteimg.com/img/user-avatar/585e1491713363bc8f67d06c485e8260~200x200.awebp",
+    banner: "Ronny Zhang‘s Blog",
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "prismjs/themes/prism-okaidia.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import { DefaultSeo } from "next-seo";
+import { config } from "../config";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <>
+        <DefaultSeo
+            title={config.title}
+            description={config.description}
+            twitter={{
+              handle: "@handle",
+              site: "@site",
+              cardType: "summary_large_image",
+            }}
+        />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+  );
 }
+
+export default MyApp;
